06c2d2d41629fb8f793763ddb8945a6855cda3e8
michaun420 <darchon25@gmail.com>
2024-12-06 13:02:17 +0100

    06.12 2

--- src/routes/Calc.jsx
+++ src/routes/Calc.jsx
@@ -1,9 +1,40 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useContext, useRef, useState } from "react";
+
 
 function Calc(){
 
+    const [waga, setWaga] = useState('')
+    const [reps, setReps] = useState('')
+    const [max, setMax] = useState(null)
+
+    const max_rep = (e) =>{
+        e.preventDefault();
+        const parsedWaga = parseFloat(waga)
+        const parsedReps = parseFloat(reps, 10)
+        if (parsedWaga > 0 && parsedReps > 0){
+            const result = parsedWaga * parsedReps * 0.0333 + parsedWaga
+            setMax(result.toFixed(2))
+        }
+    }
+
+
+
+
+
     return <div className="main">
-        <h1>Siema eniu</h1>
+        <div className="calc">
+            <form className="form" onSubmit={max_rep}>
+            Podaj wagę:<br></br><br></br>
+            <input type='number' value={waga} onChange={(e)=>setWaga(e.target.value)}></input><br></br><br></br>
+            Podaj liczbę powtórzeń:<br></br><br></br>
+            <input type='number' value={reps} onChange={(e)=>setReps(e.target.value)}></input><br></br><br></br>
+            <button type="submit">Oblicz swojego maxa</button>
+            </form>
+            <div className="wynik">
+                Twój szacowany 1RM: <br></br>{max}
+            </div>
+        </div>
     </div>
 }
 export default Calc
